eb4d608419945b8c93d64c0ef085c79a38d43a6e
Lorenzo Sciandra <lorenzo.sciandra@gmail.com>
2017-12-03 23:02:16 +0000

    trying to guess a way to fetch the coordinates

---
+++ day3/solution-part1.js
@@ -0,0 +1,27 @@
+const puzzleInput = 1024;
+
+const whichOrbit = pointToFind => {
+  let orbit = 0;
+  let valueOfDistance = 0;
+  if (pointToFind === 1) {
+    return { orbit, valueOfDistance };
+  }
+  for (let i = 1; ; i = i + 2) {
+    console.log([Math.pow(i - 2, 2) + 1, pointToFind, Math.pow(i, 2), orbit]);
+    if (pointToFind > Math.pow(i - 2, 2) + 1 && pointToFind < Math.pow(i, 2)) {
+      const test = Math.pow(i, 2) % pointToFind;
+      if (test < orbit) {
+        valueOfDistance = test + orbit;
+      } else {
+        valueOfDistance = test;
+      }
+      break;
+    }
+    orbit++;
+  }
+  return { orbit, valueOfDistance };
+};
+
+const { orbit, valueOfDistance } = whichOrbit(puzzleInput);
+
+console.log("it's in the orbit", { orbit, valueOfDistance });
